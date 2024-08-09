69a2c78bdd1bfa8b02fcf88830ef65a5fa6fff2b
Leekesh Kumar <pagotileekeshkumar@gmail.com>
2024-08-09 14:50:27 +0530

    Initial commit of the backend code

---
+++ code-similarity-backend/src/test/java/com/example/codesimilarity/CodeSimilarityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codesimilarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSimilarityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
